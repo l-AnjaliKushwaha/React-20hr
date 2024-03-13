7c38c1cb6d805bc07500780025b8305c843f81eb
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-03-13 11:31:07 +0530

    todoApp

--- Projects/1-todo-app-v1/src/App.jsx
+++ Projects/1-todo-app-v1/src/App.jsx
@@ -1,19 +1,29 @@
-import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
-import TodoItem1 from "./components/TodoItem1";
-import TodoItem2 from "./components/TodoItem2";
-import './App.css';
-
+import AddTodo from "./components/AddTodo";
+import TodoItems from "./components/TodoItems";
+import "./App.css";
 
 function App() {
+  const todoItems = [
+    {
+      name: "Buy Milk",
+      dueDate: "4/10/2023",
+    },
+    {
+      name: "Go to College",
+      dueDate: "4/10/2023",
+    },
+    {
+      name: "Like this video",
+      dueDate: "right now",
+    },
+  ];
+
   return (
     <center className="todo-container">
-      <AppName></AppName>
-      <AddTodo></AddTodo>
-      <div className="items-container">
-        <TodoItem1></TodoItem1>
-        <TodoItem2></TodoItem2>
-      </div>
+      <AppName />
+      <AddTodo />
+      <TodoItems todoItems={todoItems}></TodoItems>
     </center>
   );
 }
--- Projects/1-todo-app-v1/src/components/AddTodo.jsx
+++ Projects/1-todo-app-v1/src/components/AddTodo.jsx
@@ -1,21 +1,21 @@
-function AddTodo(){
-            return (
-              <div class="container">
-                <div class="row kg-row">
-                  <div class="col-6">
-                    <input type="text" placeholder="Enter Todo Here" />
-                  </div>
-                  <div class="col-4">
-                    <input type="date" />
-                  </div>
-                  <div class="col-2">
-                    <button type="button" class="btn btn-success kg-button">
-                      Add
-                    </button>
-                  </div>
-                </div>
-              </div>
-            );
+function AddTodo() {
+  return (
+    <div className="container text-center">
+      <div className="row kg-row">
+        <div className="col-6">
+          <input type="text" placeholder="Enter Todo Here" />
+        </div>
+        <div className="col-4">
+          <input type="date" />
+        </div>
+        <div className="col-2">
+          <button type="button" className="btn btn-success kg-button">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AddTodo
+export default AddTodo;
